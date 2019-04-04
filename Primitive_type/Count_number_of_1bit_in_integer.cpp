4fcf43950d86d16ec2027f74c52a4d5b06d63926
yuvaram <yuvaram.aligeti@gmail.com>
2019-04-04 15:33:11 +0530

    Create Count_number_of_1bit_in_integer.cpp

---
+++ Primitive_type/Count_number_of_1bit_in_integer.cpp
@@ -0,0 +1,7 @@
+/***************************************************
+ * Author : Yuvaram Aligeti
+ * Date   : 03042019
+ * Problem: Compute the number of bits set to 1 in an
+ *          integer valued variable X.
+ *
+*****************************************************/
